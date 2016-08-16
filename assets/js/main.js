302e61739961d8da6f6a6de161f41b525a8c9a7e
strcatstudios <strcatstudios@gmail.com>
2016-08-16 16:41:04 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Directive by HTML5 UP
+	Telephasic by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,10 +7,9 @@
 (function($) {
 
 	skel.breakpoints({
-		wide: '(max-width: 1680px)',
 		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 980px)',
-		narrower: '(max-width: 840px)',
+		narrow: '(max-width: 1080px)',
+		narrower: '(max-width: 820px)',
 		mobile: '(max-width: 736px)',
 		mobilep: '(max-width: 480px)'
 	});
@@ -37,6 +36,53 @@
 					skel.breakpoint('narrower').active
 				);
 			});
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				speed: 300,
+				alignment: 'center',
+				noOpenerFade: true
+			});
+
+		// Off-Canvas Navigation.
+
+			// Navigation Button.
+				$(
+					'<div id="navButton">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							'<a href="index.html" class="link depth-0">Home</a>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'top',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#navButton, #navPanel, #page-wrapper')
+						.css('transition', 'none');
 
 	});
 
